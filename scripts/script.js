3727453f391a3e4caf3b7faf53dce6a3fb54a19a
Luc van der Heiden <lucvdheiden@hotmail.com>
2023-11-02 11:17:07 +0100

    added starry background with anim
    Randomly generates a starry background and makes stars disappear over a set period of time.

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,26 @@
-// JavaScript Document
-console.log("Howdy!");
+function createStars() {
+  const starryBackground = document.querySelector('.stars');
+  const numStars = 100; // Hoeveelheid sterren
+  const duration = 60 * 1000; // Duur van animatie
+  const interval = duration / numStars; //Interval tussen verdwijnen van sterren
+
+  for (let i = 0; i < numStars; i++) {
+    const star = document.createElement('div'); //Creeër sterren
+    star.className = 'star';
+    star.style.top = `${Math.random() * 100}%`; //Geeft de ster een willekeurige locatie
+    star.style.left = `${Math.random() * 100}%`;
+    starryBackground.appendChild(star);
+  }
+
+  const stars = document.querySelectorAll('.star');
+  stars.forEach((star, index) => {
+    setTimeout(() => {
+      star.classList.add('fading'); // We voegen de "fading" class toe om de styling aan te passen (CSS zorgt voor transition)
+      setTimeout(() => {
+        star.remove(); //Verwijder de ster
+      }, 3000); // Bepaalt hoe snel de ster verdwijnt
+    }, index * interval);
+  });
+}
+
+createStars();
